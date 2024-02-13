3bfeb8d33242e8ea09007f29d4106af7a61f85fa
nyccreator <andyfernandeznyc@gmail.com>
2024-02-13 02:48:49 -0500

    feat: connect mongodb and create user schema

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,18 @@ const app = express();
 app.use("/", require("./routes/testingRoutes"));
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+require("dotenv").config();
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+
+connectDB();
+
+mongoose.connection.once("open", () => {
+	console.log("Connected to MongoDB");
+	app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+	console.log(err);
+});
