b7092770e1ef445dc22a4bda31ee512ad63fb8b4
caileanhtuan <connorcai180@gmail.com>
2024-02-12 21:53:51 -0500

    fix: node to nodemon

--- backend/controllers/testController.js
+++ backend/controllers/testController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const getTest = asyncHandler(async (req, res) => {
-  res.status(200).json({ success: "true", message: "testing API 3" });
+  res.status(200).json({ success: "true", message: "testing API 2" });
 });
 
 module.exports = { getTest };
