39176b24b315b4ceccb8c20659f4b5f01fc800ce
Hebe3834 <hebeluna00@gmail.com>
2024-02-16 21:54:40 -0500

    started signin and signup frontend with mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TestComponent } from "./components";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TestComponent />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
