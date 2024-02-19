1136e1c8521269002cc1194feee1057de388b3bf
nyccreator <andyfernandeznyc@gmail.com>
2024-02-19 13:51:15 -0500

    feat: add authentication routes and controllers and add login rate limiting middleware

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
-app.use(express.json());
-app.use("/", require("./routes/testingRoutes"));
-app.use("/users", require("./routes/userRoutes"));
-
-const PORT = process.env.PORT || 5001;
-
-require("dotenv").config();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5001;
 
 connectDB();
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/testingRoutes"));
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/users", require("./routes/userRoutes"));
 
 mongoose.connection.once("open", () => {
 	console.log("Connected to MongoDB");
