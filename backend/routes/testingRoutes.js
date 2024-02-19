01b68957ee5dda320985a1bdf9920fe1e24ca987
Xavier <xaviercarnold@gmail.com>
2024-02-19 15:44:41 -0500

    google sign on integration

--- backend/routes/testingRoutes.js
+++ backend/routes/testingRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const testingController = require("../controllers/testController");
 
-router.route("/").get(testingController.getTest);
+router.route("/testing").get(testingController.getTest);
 
 module.exports = router;
