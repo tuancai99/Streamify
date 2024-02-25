6d004c249b0306580a8eba4c2dc0c058de633923
Tuan Cai <93027364+tuancai99@users.noreply.github.com>
2024-02-25 11:39:18 -0500

    Revert "started signin and signup frontend with mui"

--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -3,7 +3,7 @@ import "../styles/TestComponent.css";
 const TestComponent = () => {
   return (
     <div>
-      <h1 className="title">This is a Testing 6</h1>
+      <h1 className="title">This is a Testing 2</h1>
       <p>This is just a simple test component to demonstrate React.</p>
     </div>
   );
