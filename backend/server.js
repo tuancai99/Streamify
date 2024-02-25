8794a0b01237d6ae0a6e7e7c2689b8a40fc2d0a1
Moses Adewolu <MosesOluwatobiAdewolu@gmail.com>
2024-02-25 16:14:00 -0500

    New Changes Made

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,4 @@
 require("dotenv").config();
-<<<<<<< HEAD
-const express = require("express");
-const app = express();
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const corsOptions = require("./config/corsOptions");
-const connectDB = require("./config/dbConn");
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5001;
-=======
 const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 const express = require("express");
@@ -17,6 +6,11 @@ const app = express();
 const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,7 +21,6 @@ app.use(session({
     cookie: { secure: false } // set to true if using https
 }));
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
@@ -35,12 +28,7 @@ app.use("/", require("./routes/testingRoutes"));
 app.use("/users", require("./routes/userRoutes"));
 app.use("/", require("./routes/authRoutes"));
 
-
 const PORT = process.env.PORT || 5001;
-
-
-
->>>>>>> 6ffd3f85497ac4f1272e7ee6206677d8086c7bd6
 
 connectDB();
 
