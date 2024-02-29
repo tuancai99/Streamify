31744d2f27e38b78022dc9a549267607f64be26f
caileanhtuan <connorcai180@gmail.com>
2024-02-28 20:01:48 -0500

    fix: add navigation

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,59 +1,105 @@
 import React from "react";
 import "../styles/TestComponent.css";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { TextField } from '@mui/material';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import Divider from '@mui/material/Divider';
-import { styled } from '@mui/material/styles';
-
-const Root = styled('div')(({ theme }) => ({
-    width: '100%',
-    ...theme.typography.body2,
-    color: theme.palette.text.secondary,
-    '& > :not(style) ~ :not(style)': {
-      marginTop: theme.spacing(2),
-    },
-  }));
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Unstable_Grid2";
+import Divider from "@mui/material/Divider";
+import { styled } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+const Root = styled("div")(({ theme }) => ({
+  width: "100%",
+  ...theme.typography.body2,
+  color: theme.palette.text.secondary,
+  "& > :not(style) ~ :not(style)": {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const ToSignIn = () => {
+    navigate("/signin");
+  };
   return (
-    <Grid container justifyContent="center" alignContent="center" height="100vh" bgcolor={"#FF8652"}>
-        <Card direction="column"  sx={{ maxWidth: 500}} style={{ padding: 30 }} >
-            <ArrowBackIcon />
-            <Stack direction="column" alignItems="center" gap={1}>
-                <CardContent className="cardContentStyle" sx={{width: 300}}>
-                    <Grid 
-                        container
-                        direction="column"
-                        justify="center"
-                        alignItems="center"
-                        gap={2}
-                    >
-                    <AccountCircleIcon />
-                    <Typography variant="h2" sx={{ fontSize: 30, textAlign:"center" }} >Let's Sign Up</Typography>
-                    
-                    <TextField id="email" label="Enter E-mail" variant="outlined" fullWidth/>
-                    <Stack direction="row" gap={1}>
-                      <TextField id="first-name" label="Enter First Name" variant="outlined" fullWidth/>
-                      <TextField id="last-name" label="Enter Last Name" variant="outlined" fullWidth/>
-                    </Stack>
-                    <TextField id="password" label="Enter Password" variant="outlined" fullWidth/>
-                    <TextField id="password-confirm" label="Re-Enter Password" variant="outlined" fullWidth/>
+    <Grid
+      container
+      justifyContent="center"
+      alignContent="center"
+      height="100vh"
+      bgcolor={"#FF8652"}
+    >
+      <Card direction="column" sx={{ maxWidth: 500 }} style={{ padding: 30 }}>
+        <ArrowBackIcon sx={{ cursor: "pointer" }} onClick={ToSignIn} />
+        <Stack direction="column" alignItems="center" gap={1}>
+          <CardContent className="cardContentStyle" sx={{ width: 300 }}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              gap={2}
+            >
+              <AccountCircleIcon />
+              <Typography
+                variant="h2"
+                sx={{ fontSize: 30, textAlign: "center" }}
+              >
+                Let's Sign Up
+              </Typography>
 
-                    <Root><Divider /></Root>
-                    <Button variant="contained" style={{backgroundColor: "#26E5B7", color: "#2D2C2F"}} fullWidth>Sign Up</Button>
+              <TextField
+                id="email"
+                label="Enter E-mail"
+                variant="outlined"
+                fullWidth
+              />
+              <Stack direction="row" gap={1}>
+                <TextField
+                  id="first-name"
+                  label="Enter First Name"
+                  variant="outlined"
+                  fullWidth
+                />
+                <TextField
+                  id="last-name"
+                  label="Enter Last Name"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Stack>
+              <TextField
+                id="password"
+                label="Enter Password"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="password-confirm"
+                label="Re-Enter Password"
+                variant="outlined"
+                fullWidth
+              />
 
-                </Grid>
-                </CardContent>
-            </Stack>
-        </Card>
+              <Root>
+                <Divider />
+              </Root>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#26E5B7", color: "#2D2C2F" }}
+                fullWidth
+              >
+                Sign Up
+              </Button>
+            </Grid>
+          </CardContent>
+        </Stack>
+      </Card>
     </Grid>
   );
 };
