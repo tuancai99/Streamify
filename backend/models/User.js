9980ccc2abbb79b91cce63752d2d00005cd28632
nyccreator <andyfernandeznyc@gmail.com>
2024-04-07 10:08:11 -0400

    feat: create message schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,48 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	username: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	name: {
-		first: {
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		name: {
+			first: {
+				type: String,
+				required: true,
+			},
+			last: {
+				type: String,
+				required: true,
+			},
+		},
+		password: {
 			type: String,
 			required: true,
 		},
-		last: {
+		pictureProfile: {
 			type: String,
-			required: true,
+			default: "",
+		},
+		admin: {
+			type: Boolean,
+			default: false,
+		},
+		active: {
+			type: Boolean,
+			default: true,
+		},
+		googleId: {
+			type: String,
+			unique: true,
+			default: "",
+			partialFilterExpression: { googleId: { $type: "string" } },
 		},
 	},
-	password: {
-		type: String,
-		required: true,
-	},
-	pictureProfile: {
-		type: String,
-		default: "",
-	},
-	admin: {
-		type: Boolean,
-		default: false,
-	},
-	active: {
-		type: Boolean,
-		default: true,
-	},
-	googleId: {
-		type: String,
-		unique: true,
-		default: "",
-		partialFilterExpression: { googleId: { $type: "string" } },
-	},
-});
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
